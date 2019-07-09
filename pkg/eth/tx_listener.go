dfcf083f6aeadb57cad442c1d7dfde10d6f85a2a
walfly <walker.d.flynn@gmail.com>
2019-07-09 12:20:09 -0500

    CIVIL-901 tx listener retries for a period of time to wait for tx to get into pool

--- pkg/eth/tx_listener.go
+++ pkg/eth/tx_listener.go
@@ -3,6 +3,7 @@ package eth
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	ethereum "github.com/ethereum/go-ethereum"
@@ -27,15 +28,27 @@ const (
 	txListenerPrefix = "TxListener"
 )
 
+type waiter struct {
+	Mutex       *sync.Mutex
+	DoneWaiting bool
+	QuitChan    chan bool
+}
+
 // TxListener provides methods to interact with Ethereum transactions
 type TxListener struct {
 	blockchain ethereum.TransactionReader
 	jobs       jobs.JobService
+	timeout    time.Duration
+}
+
+// NewTxListenerWithWaitPeriod creates a new TransactionService instance with a wait period
+func NewTxListenerWithWaitPeriod(blockchain ethereum.TransactionReader, jobs jobs.JobService, timeout time.Duration) *TxListener {
+	return &TxListener{blockchain, jobs, timeout}
 }
 
 // NewTxListener creates a new TransactionService instance
 func NewTxListener(blockchain ethereum.TransactionReader, jobs jobs.JobService) *TxListener {
-	return &TxListener{blockchain, jobs}
+	return &TxListener{blockchain, jobs, 0 * time.Second}
 }
 
 // StartListener begins listening for an ethereum transaction
@@ -68,6 +81,13 @@ func (t *TxListener) StopSubscription(receipt *jobs.Subscription) error {
 
 // PollForTxCompletion will continuously poll until a transaction is complete
 func (t *TxListener) PollForTxCompletion(txID string, updates chan<- string) {
+	w := &waiter{
+		Mutex:       &sync.Mutex{},
+		DoneWaiting: false,
+		QuitChan:    make(chan bool),
+	}
+	go t.waitForTimeout(w)
+	defer close(w.QuitChan)
 
 	hash := common.HexToHash(txID)
 
@@ -77,6 +97,12 @@ func (t *TxListener) PollForTxCompletion(txID string, updates chan<- string) {
 		isPending, err := t.checkTx(hash)
 		if err != nil {
 			updates <- fmt.Sprintf("%v %v", TxListenerTransactionErrorMsgPrefix, err.Error())
+			w.Mutex.Lock()
+			if err == ethereum.NotFound && !w.DoneWaiting {
+				w.Mutex.Unlock()
+				continue
+			}
+			w.Mutex.Unlock()
 			return
 		}
 		if !isPending {
@@ -88,8 +114,20 @@ func (t *TxListener) PollForTxCompletion(txID string, updates chan<- string) {
 
 }
 
-func (t *TxListener) checkTx(hash common.Hash) (bool, error) {
+func (t *TxListener) waitForTimeout(w *waiter) {
+	timer := time.NewTimer(t.timeout)
+	select {
+	case <-timer.C:
+		w.Mutex.Lock()
+		w.DoneWaiting = true
+		w.Mutex.Unlock()
+	case <-w.QuitChan:
+		log.Infof("Quitting waiting for transaction to appear because it was found or there was another error\n")
+		timer.Stop()
+	}
+}
 
+func (t *TxListener) checkTx(hash common.Hash) (bool, error) {
 	_, isPending, err := t.blockchain.TransactionByHash(context.Background(), hash)
 	if err != nil {
 		log.Errorf("Error retrieving TransactionByHash: err: %v\n", err)
